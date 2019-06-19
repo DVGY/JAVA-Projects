5c569a699b4cab0fc2afc0d06b8ab8731e955062
Gaurav Yadav <Gaurav Yadav@Gaurav-PC>
2019-06-19 15:13:17 +0530

    Fourth Commmit All button fucntionality added and working. Recursive Operation remaining to code.

--- Calculator/src/main/java/gproject/calculator/FXMLController.java
+++ Calculator/src/main/java/gproject/calculator/FXMLController.java
@@ -12,8 +12,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
+
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private HBox hboxView;
 
@@ -46,7 +47,7 @@ public class FXMLController implements Initializable {
 
     @FXML
     private Button zero;
-    
+
     @FXML
     private Button one;
 
@@ -55,7 +56,7 @@ public class FXMLController implements Initializable {
 
     @FXML
     private Button three;
-    
+
     @FXML
     private Button four;
 
@@ -64,22 +65,22 @@ public class FXMLController implements Initializable {
 
     @FXML
     private Button six;
-    
+
     @FXML
     private Button seven;
-    
+
     @FXML
     private Button Eight;
-    
+
     @FXML
     private Button nine;
-    
+
     @FXML
     private Button percentage;
 
     @FXML
     private Button decimal;
-    
+
     @FXML
     private Button squareRoot;
 
@@ -107,89 +108,176 @@ public class FXMLController implements Initializable {
     @FXML
     private Button cos;
     Model modelObj = new Model();
-    
+
     private float numberOne;
     private float numberTwo;
     private float result;
     boolean flag = true;
     boolean sucessiveOperation = false;
-    boolean afterOperatorPressed= false;
-    String numericalValue="";
-    String operator="";
+    boolean afterOperatorPressed = false;
+    boolean inFunctionModule = false;
+    int inFucntionState;
+    String numericalValue = "";
+    String operator = "";
 
     @FXML
-    private void handleNumberButtonAction(ActionEvent event) 
-    {
-       if("0".equals(textView.getText()))
-       {
-        textView.setText("");
-        numericalValue=((Button)event.getSource()).getText();
-        textView.setText(numericalValue);
-       }
-       else if(afterOperatorPressed)
-               {
-                   textView.setText("");
-                   numericalValue=((Button)event.getSource()).getText();
-                   numericalValue =textView.getText()+numericalValue;
-                   textView.setText(numericalValue);
-                   afterOperatorPressed =false;
-               }
-       else
-       {
-         numericalValue=((Button)event.getSource()).getText();
-         numericalValue =textView.getText()+numericalValue;
-         textView.setText(numericalValue);
-         
-       }     
-    }
-    @FXML
-    private void handleOperatorButtonAction(ActionEvent event)
-    {
-        
-        if(sucessiveOperation)
+    private void handleNumberButtonAction(ActionEvent event) {
+        if ("0".equals(textView.getText())) {
+            textView.setText("");
+            numericalValue = ((Button) event.getSource()).getText();
+            textView.setText(numericalValue);
+        } else if (afterOperatorPressed) {
+            textView.setText("");
+            numericalValue = ((Button) event.getSource()).getText();
+            numericalValue = textView.getText() + numericalValue;
+            textView.setText(numericalValue);
+            afterOperatorPressed = false;
+            
+        }   
+        else if(inFunctionModule)
         {
-            numberOne=result;
-            flag =false;
+         textView.setText("");
+         numericalValue = ((Button) event.getSource()).getText();
+         numericalValue = textView.getText() + numericalValue;
+         textView.setText(numericalValue);   
+         inFunctionModule = false;   
         }
         
-       if(flag)
-       {
-       numberOne = Float.parseFloat(numericalValue);
-       numericalValue= "";
-       flag= false;
-       }
-       operator = ((Button)event.getSource()).getText();
-        
-       textView.setText(operator); 
-       afterOperatorPressed= true; 
-        
-        
-        
+         else {
+            numericalValue = ((Button) event.getSource()).getText();
+            numericalValue = textView.getText() + numericalValue;
+            textView.setText(numericalValue);
+
+        }
     }
+
     @FXML
-    private void handleFunctionButtonAction(ActionEvent event)
-    {
-        textView.setText("In function");
+    private void handleOperatorButtonAction(ActionEvent event) {
+
+        if (sucessiveOperation) {
+            numberOne = result;
+            flag = false;
+        }
+
+        if (flag) {
+            numberOne = Float.parseFloat(numericalValue);
+            numericalValue = "";
+            flag = false;
+        }
+        operator = ((Button) event.getSource()).getText();
+
+        textView.setText(operator);
+        afterOperatorPressed = true;
+
     }
+
     @FXML
-    private void handlerResultAction(ActionEvent event)
-    {
-       
-        if(!flag)
-       {
-       numberTwo = Float.parseFloat(numericalValue);
-       numericalValue= "";
-       flag = true;
-       }
-        result= modelObj.compute(numberOne, numberTwo, operator);
+    private void handleClearButtonAction(ActionEvent event) {
+        if ("cancel".equals(((Button) event.getSource()).getId())) {
+            if ("0".equals(textView.getText())) {
+                textView.setText("");
+            } else if (flag == true) {
+                int len = numericalValue.length();
+                numericalValue = numericalValue.substring(0, len - 1);
+                textView.setText(numericalValue);
+            } else if (flag == false && (afterOperatorPressed == true)) {
+                operator = "";
+                textView.setText("");
+            } else if ((flag == false) && (afterOperatorPressed == false)) {
+                int len = numericalValue.length();
+                numericalValue = numericalValue.substring(0, len - 1);
+                textView.setText(numericalValue);
+            }
+        } else {
+            textView.setText("");
+            numberOne = 0;
+            operator = "";
+            numberTwo = 0;
+            result=0;
+        }
+    }
+
+    @FXML
+    private void handleFunctionButtonAction(ActionEvent event) {
+        String buttonId;
+        inFunctionModule = true;
+        inFucntionState=1;
+        
+        buttonId = ((Button) event.getSource()).getId();
+        switch (buttonId) {
+            case "squareRoot":
+                if(sucessiveOperation)
+                {
+                    numberOne=result;
+                    textView.setText("sqrt("+ result+")");
+                    operator = buttonId;
+                }
+                else
+                {
+                    textView.setText("sqrt(");
+                    operator = buttonId;
+                }
+                break;
+            case "sin":
+                textView.setText("sin(");
+                operator=buttonId;
+                break;
+            case "cos":
+                textView.setText("cos(");
+                operator=buttonId;
+                break;
+            case "tan":
+                textView.setText("tan(");
+                operator=buttonId;
+                break;
+            case "xInverse":
+                textView.setText("x INV(");
+                operator=buttonId;
+                break;
+            case "xSquare":
+                textView.setText("square(");
+                operator=buttonId;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    @FXML
+    private void handlerResultAction(ActionEvent event) {
+
+        if (!flag) {
+            if (numericalValue.isEmpty()) {
+                numberTwo = 0;
+            } else {
+                numberTwo = Float.parseFloat(numericalValue);
+                numericalValue = "";
+                flag = true;
+            }
+        }
+        
+        if ((!inFunctionModule) && (inFucntionState==1)) {
+            numberTwo=0;
+            if(numericalValue.isEmpty())
+            {
+                numberOne=0;
+            }
+            else
+            {
+            numberOne=Float.parseFloat(numericalValue);
+            
+            }
+        }  
+        result = modelObj.compute(numberOne, numberTwo, operator);
         String dispResult = Float.toString(result);
         textView.setText(dispResult);
-        sucessiveOperation=true;
-        
+        sucessiveOperation = true;
+       
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
--- Calculator/src/main/java/gproject/calculator/Model.java
+++ Calculator/src/main/java/gproject/calculator/Model.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package gproject.calculator;
+import java.lang.Math;
 
 /**
  *
@@ -26,10 +27,30 @@ public class Model {
                     result= numOne*numTwo;
                     break;
             case "/":
-                     
                     result= numOne/numTwo;
                     break;
-                   
+            case "%":
+                    result= (float)(numOne*(0.01));
+                    break;        
+            case "cos":
+                    result=(float)Math.cos(numOne);
+                    break;
+            case "sin":
+                    result=(float)Math.sin(numOne);
+                    break;        
+            case "tan":
+                    result=(float)Math.tan(numOne);
+                    break;
+            case "squareRoot":
+                    result=(float)Math.sqrt(numOne);
+                    break;       
+            case "xInverse":
+                    result=(float)Math.pow(numOne,-1);
+                    break; 
+            case "xSquare":
+                    result=(float)Math.pow(numOne,2);
+                    break; 
+                    
             default: 
                 return 0;
                   
